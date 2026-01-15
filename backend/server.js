5ffc47df1848703413f07e23dc708e3e9c32f5cb
Bipul Singhaniya <singhaniyabipul9@gmail.com>
2026-01-15 22:20:32 +0530

    added

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,25 @@
+// require("dotenv").config();
+// const express = require("express");
+// const cors = require("cors");
+// const connectDB = require("./config/db");
+
+// connectDB();
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+
+// app.use("/api/auth", require("./routes/authRoutes"));
+// app.use("/api/questions", require("./routes/questionRoutes"));
+// app.use("/api/admin", require("./routes/adminRoutes"));
+
+// app.listen(process.env.PORT, () =>
+//   console.log("🚀 Server running on port", process.env.PORT)
+// );
+
+
+
+
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
@@ -6,15 +28,19 @@ const connectDB = require("./config/db");
 connectDB();
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/questions", require("./routes/questionRoutes"));
 app.use("/api/admin", require("./routes/adminRoutes"));
 
-app.listen(process.env.PORT, () =>
-  console.log("🚀 Server running on port", process.env.PORT)
-);
+const PORT = process.env.PORT || 5000;
 
+app.listen(PORT, () => {
+  console.log("🚀 Server running on port", PORT);
+});
 
