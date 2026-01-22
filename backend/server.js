e24731c6aeb9a2ccd1a727a51848b0a31c2e7796
Bipul Singhaniya <singhaniyabipul9@gmail.com>
2026-01-22 20:03:18 +0530

    asdf

--- backend/server.js
+++ backend/server.js
@@ -36,17 +36,15 @@ connectDB();
 
 const app = express();
 
-// app.use(cors({
-//   origin: "http://localhost:5173",
-//   credentials: true
-// }));
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: "https://lpuquora-frontend.onrender.com",
     credentials: true,
   })
 );
+
+
 
 
 app.use(express.json());
