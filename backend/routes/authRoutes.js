403bd36dee64a0e705970fc95059f57178d19aab
Bipul Singhaniya <singhaniyabipul9@gmail.com>
2026-01-26 11:28:31 +0530

    asdf

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,20 +1,20 @@
 
 
-// const router = require("express").Router();
-// const { register, login } = require("../controllers/authController");
+const router = require("express").Router();
+const { register, login } = require("../controllers/authController");
 
-// router.post("/register", register);
-// router.post("/login", login);
+router.post("/register", register);
+router.post("/login", login);
 
-// module.exports = router;
+module.exports = router;
 
 
 
 /// mai 
 
-const router = require("express").Router();
-const { register, login, verifyEmail } = require("../controllers/authController");
-router.get("/verify/:token", verifyEmail);
-router.post("/register", register);
-router.post("/login", login);
-module.exports = router;
+// const router = require("express").Router();
+// const { register, login, verifyEmail } = require("../controllers/authController");
+// router.get("/verify/:token", verifyEmail);
+// router.post("/register", register);
+// router.post("/login", login);
+// module.exports = router;
