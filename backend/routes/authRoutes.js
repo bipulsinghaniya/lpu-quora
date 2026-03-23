918a5d09c55e1dbdb6fcb190eac75681176c45de
Bipul Singhaniya <singhaniyabipul9@gmail.com>
2026-03-23 18:09:46 +0530

    asd

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -7,14 +7,3 @@ router.post("/register", register);
 router.post("/login", login);
 
 module.exports = router;
-
-
-
-/// mai 
-
-// const router = require("express").Router();
-// const { register, login, verifyEmail } = require("../controllers/authController");
-// router.get("/verify/:token", verifyEmail);
-// router.post("/register", register);
-// router.post("/login", login);
-// module.exports = router;
